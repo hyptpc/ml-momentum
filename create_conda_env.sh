47e3ce6c2f91e540f9a40fb4bb277a35c206c441
rjsaito0519 <ryuta.saito.q3@dc.tohoku.ac.jp>
2024-03-12 01:40:54 +0900

    minor change

--- create_conda_env.sh
+++ create_conda_env.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 conda create -n ml-mom python=3.10 -y
+conda init bash
 conda activate ml-mom
 conda install -y numpy psutil pyyaml rich matplotlib tqdm statistics
 conda install -y pytorch torchvision torchaudio cpuonly -c pytorch
